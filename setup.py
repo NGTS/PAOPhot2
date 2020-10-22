51dbcaf50999960ee74d1a21e8d4bbc93c5e1a52
sam <samgill844@gmail.com>
2020-10-22 16:02:59 +0100

    First

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     #package_data={'spec1d': ['data/gr5_-4_ref_spectra.dat', 'data/gr5_-4_ref_lines.dat']},
     #package_data={'tessffiextract': ['data/*.csv']},
     scripts=['Utils/reduce_action', 'Utils/find_best_comparisons', 'Utils/make_photometry_catalogue', 'Utils/postprocess_action', 'Utils/photpack',
-            'Utils/process_night'],
+            'Utils/process_night', 'Utils/process_night_'],
     #install_requires=['numba']
 )
